e1c6a855944412d20968d868db71ea311f42e782
mariaeduardaaaaaaaa <146455966+mariaeduardaaaaaaaa@users.noreply.github.com>
2025-05-16 10:58:07 -0300

    16/05

--- main.js
+++ main.js
@@ -2,6 +2,12 @@ const botoes = document.querySelector( ".botao");
 
 for(let i=0; i<botoes.clientHeight; i++){
     botoes[i].onclick = function(){
+
+for(let j=0;j<botoes.clientHeight;j++){ 
+    botoes[j].classList.remove("ativo")
+}
+
         botoes[i].classList.add("ativo");
     }
 }
+
